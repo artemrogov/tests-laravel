0a776110347576e3f5c05bf6fadd882af69f02aa
artemrogov <artemrogov281@gmail.com>
2021-08-16 20:13:04 +0000

    orhid install && customize presets

---
+++ resources/views/vendor/platform/footer.blade.php
@@ -0,0 +1,11 @@
+@guest
+
+@else
+    <div class="text-center user-select-none">
+        <p class="small m-0">
+            <a href="http://orchid.software" target="_blank" rel="noopener">
+                Подвал административной панели
+            </a>
+        </p>
+    </div>
+@endguest
